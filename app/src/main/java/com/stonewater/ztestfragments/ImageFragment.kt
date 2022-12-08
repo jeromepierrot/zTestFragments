d6cd863afec2fd1e9f4e13910a06bfbda4d6d68c
jpierrot <je.pierrot@gmail.com>
2022-12-08 18:19:14 +0100

    add TextFragment (layout + code) + add ImageFragment (layout) : TODO code + add 2 buttons in MainActivity to load either TextFragment or ImageFragment content inside

--- app/src/main/java/com/stonewater/ztestfragments/ImageFragment.kt
+++ app/src/main/java/com/stonewater/ztestfragments/ImageFragment.kt
@@ -1,5 +1,6 @@
 package com.stonewater.ztestfragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -12,48 +13,40 @@ private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 /**
- * A simple [Fragment] subclass.
- * Use the [ImageFragment.newInstance] factory method to
- * create an instance of this fragment.
+ * A simple [Fragment] subclass showing up a picture from drawable resources
  */
 class ImageFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_image, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ImageFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ImageFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
--- app/src/main/java/com/stonewater/ztestfragments/MainActivity.kt
+++ app/src/main/java/com/stonewater/ztestfragments/MainActivity.kt
@@ -2,10 +2,22 @@ package com.stonewater.ztestfragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_text.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadTextBtn.setOnClickListener{
+            val textFragment = TextFragment()
+            val manager = supportFragmentManager
+            val transaction = manager.beginTransaction()
+
+            transaction.replace(R.id.fragment_container, textFragment)
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
     }
 }
